26cbfd7498b8678e2a8727c02425dfa5f1cdf8ac
dave2041 <davidmstammers@gmail.com>
2016-10-27 09:49:03 +0100

    Initial Commit
    Test App commited to GIT - A simple multiform example

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/FormManager.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    public class FormManager
+    {
+        Form1 f1;
+        Retail f2;
+        Time f3;
+        public void initialize()
+        {
+            f2 = new Retail();
+            f3 = new Time();
+            f2.SetFM(this);
+            f3.SetFM(this);
+        }
+
+        public void SetMenuForm(Form1 f)
+        {
+            f1 = f;
+        }
+        public void ShowMenu()
+        {
+            f2.Show();
+            f3.Hide();
+            f1.Hide();
+        }
+
+        public void ShowTime()
+        {
+            f1.Show();
+            f2.Hide();
+            f3.Hide();
+        }
+
+        public void ShowRetail()
+        {
+            f3.Show();
+            f2.Hide();
+            f1.Hide();
+        }
+    }
+}
